f9dbb1862f119fcbd50c35b9a2d46d999660ee25
dj4t9n <pandeyjatin47@gmail.com>
2021-11-27 21:00:55 +0530

    remove comments and add types

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export const activate = (context: vscode.ExtensionContext) : void => {
 	console.log('Congratulations, your extension "goko" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('goko.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable: vscode.Disposable = vscode.commands.registerCommand('goko.helloWorld', () => {
 		vscode.window.showInformationMessage('This is my first vscode extension');
 	});
 
@@ -24,7 +12,6 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable, disposable2);
-}
+};
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export const deactivate = () : void => {};
